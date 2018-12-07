8ee8bbaa29249941dde053cd420b90e849d5c74c
Bineesh Lazar <bineesh@spanidea.com>
2018-12-07 14:16:15 +0530

    working

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"flag"
 	"fmt"
@@ -9,75 +8,15 @@ import (
 
 	logging "github.com/ipfs/go-log"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-discovery"
 	libp2pdht "github.com/libp2p/go-libp2p-kad-dht"
-	inet "github.com/libp2p/go-libp2p-net"
-	"github.com/libp2p/go-libp2p-peerstore"
-	"github.com/libp2p/go-libp2p-protocol"
 	"github.com/multiformats/go-multiaddr"
 )
 
 var log = logging.Logger("rendezvous")
 
-func handleStream(stream inet.Stream) {
-	log.Info("Got a new stream!")
-
-	// Create a buffer stream for non blocking read and write.
-	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-
-	go readData(rw)
-	go writeData(rw)
-
-	// 'stream' will stay open until you close it (or the other side closes it).
-}
-
-func readData(rw *bufio.ReadWriter) {
-	for {
-		str, err := rw.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from buffer")
-			panic(err)
-		}
-
-		if str == "" {
-			return
-		}
-		if str != "\n" {
-			// Green console colour: 	\x1b[32m
-			// Reset console colour: 	\x1b[0m
-			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
-		}
-
-	}
-}
-
-func writeData(rw *bufio.ReadWriter) {
-	stdReader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("> ")
-		sendData, err := stdReader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading from stdin")
-			panic(err)
-		}
-
-		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
-		if err != nil {
-			fmt.Println("Error writing to buffer")
-			panic(err)
-		}
-		err = rw.Flush()
-		if err != nil {
-			fmt.Println("Error flushing buffer")
-			panic(err)
-		}
-	}
-}
-
 func main() {
 	help := flag.Bool("h", false, "Display Help")
-	config, err := ParseFlags()
+	config, err := parseFlags()
 	if err != nil {
 		panic(err)
 	}
@@ -103,11 +42,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Host created. We are:", host.ID())
-
-	// Set a function as stream handler.
-	// This function is called when a peer initiates a connection and starts a stream with this peer.
-	host.SetStreamHandler(protocol.ID(config.ProtocolID), handleStream)
+	fmt.Println("Host created. We are:", host.ID().Pretty())
 
 	// Start a DHT, for use in peer discovery. We can't just make a new DHT client
 	// because we want each peer to maintain its own local copy of the DHT, so
@@ -124,56 +59,6 @@ func main() {
 	if err = kademliaDHT.Bootstrap(ctx); err != nil {
 		panic(err)
 	}
-
-	// Let's connect to the bootstrap nodes first. They will tell us about the other nodes in the network.
-	for _, peerAddr := range config.BootstrapPeers {
-		peerinfo, _ := peerstore.InfoFromP2pAddr(peerAddr)
-
-		if err := host.Connect(ctx, *peerinfo); err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println("Connection established with bootstrap node:", *peerinfo)
-		}
-	}
-
-	// We use a rendezvous point "meet me here" to announce our location.
-	// This is like telling your friends to meet you at the Eiffel Tower.
-	fmt.Println("Announcing ourselves...")
-	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
-	discovery.Advertise(ctx, routingDiscovery, config.RendezvousString)
-	fmt.Println("Successfully announced!")
-
-	// Now, look for others who have announced
-	// This is like your friend telling you the location to meet you.
-	fmt.Println("Searching for other peers...")
-	peerChan, err := routingDiscovery.FindPeers(ctx, config.RendezvousString)
-	if err != nil {
-		panic(err)
-	}
-
-	go func() {
-		for peer := range peerChan {
-			if peer.ID == host.ID() {
-				continue
-			}
-			fmt.Println("Found peer:", peer)
-
-			fmt.Println("Connecting to:", peer)
-			stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
-
-			if err != nil {
-				fmt.Println("Connection failed:", err)
-				continue
-			} else {
-				rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
-
-				go writeData(rw)
-				go readData(rw)
-			}
-
-			fmt.Println("Connected to:", peer)
-		}
-	}()
 
 	select {}
 }
--- bootstrap/flags.go
+++ bootstrap/flags.go
@@ -27,48 +27,17 @@ func (al *addrList) Set(value string) error {
 	return nil
 }
 
-// IPFS bootstrap nodes. Used to find other peers in the network.
-var defaultBootstrapAddrStrings = []string{
-	"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
-	"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
-	"/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
-	"/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
-	"/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
-}
-
-func StringsToAddrs(addrStrings []string) (maddrs []maddr.Multiaddr, err error) {
-	for _, addrString := range addrStrings {
-		addr, err := maddr.NewMultiaddr(addrString)
-		if err != nil {
-			return maddrs, err
-		}
-		maddrs = append(maddrs, addr)
-	}
-	return
-}
-
+// Config contains bootstrap configuration
 type Config struct {
-	RendezvousString string
-	BootstrapPeers   addrList
-	ListenAddresses  addrList
-	ProtocolID       string
+	ListenAddresses addrList
+	ProtocolID      string
 }
 
-func ParseFlags() (Config, error) {
+func parseFlags() (Config, error) {
 	config := Config{}
-	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me here", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
-	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
 	flag.Var(&config.ListenAddresses, "listen", "Adds a multiaddress to the listen list")
 	flag.StringVar(&config.ProtocolID, "pid", "/chat/1.1.0", "Sets a protocol id for stream headers")
 	flag.Parse()
-
-	if len(config.BootstrapPeers) == 0 {
-		bootstrapPeerAddrs, err := StringsToAddrs(defaultBootstrapAddrStrings)
-		if err != nil {
-			return config, err
-		}
-		config.BootstrapPeers = bootstrapPeerAddrs
-	}
 
 	return config, nil
 }
